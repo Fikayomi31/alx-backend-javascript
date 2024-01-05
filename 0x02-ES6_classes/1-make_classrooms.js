9ce1d77b11addb281f19000c9e92e9449105b41e
Fikayomi31 <fikayoogundijo@gmail.com>
2024-01-05 01:51:28 +0000

    make classroom

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,6 +1,6 @@
 import ClassRoom from './0-classroom';
 
-export default function initialzeRoom () {
+export default function initialzeRoom() {
   const roomSize = [19, 20, 34];
   return roomSize.map((value) => new ClassRoom(value));
 }
