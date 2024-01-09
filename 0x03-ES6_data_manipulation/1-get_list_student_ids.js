ebb8c7096fd022eea99bb26810e3567f67b04306
Fikayomi31 <fikayoogundijo@gmail.com>
2024-01-09 15:52:18 +0000

    filter

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -2,5 +2,5 @@ export default function getListStudentIds(studentList) {
   if (!Array.isArray(studentList)) {
     return [];
   }
-  return studentList.map((studentList) => studentList.id);
+  return studentList.map((student) => student.id);
 }
