4dd48a9fbe8b0f2ea36477790e2f40e9d24cedb6
Fikayomi31 <fikayoogundijo@gmail.com>
2024-02-16 23:34:42 +0000

    Iterate through object

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,4 +1,4 @@
-export default function iterateThroughObject (reportWithIterator) {
+export default function iterateThroughObject(reportWithIterator) {
   const result = [];
   for (const idx of reportWithIterator) {
     result.push(idx);
