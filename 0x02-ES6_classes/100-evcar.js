da307372de359c6df8a9d0fbc87a8494a8d356b9
Fikayomi31 <fikayoogundijo@gmail.com>
2024-02-22 02:08:44 +0000

    evcar

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,11 +1,12 @@
-import Car from './10-car.js';
+import Car from './10-car';
 
 export default class EvCar extends Car {
-	constructor(brand, motor, color, range) {
-		super(brand, motor, color);
-		this._range = range;
-	}
-	cloneCar() {
-		return new Car;
-	}
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  cloneCar() {
+    return new Car(this._brand, this._motor, this._color);
+  }
 }
