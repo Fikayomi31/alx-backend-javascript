c1645238787b795491b60f142dbb9084105cd684
Fikayomi31 <fikayoogundijo@gmail.com>
2024-03-14 00:02:58 +0000

    more map data structure

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,4 +1,4 @@
-export default function updateUniqueItems (map) {
+export default function updateUniqueItems(map) {
   if (!(map instanceof Map)) throw new Error('Cannot process');
   for (const [k, v] of map.entries()) {
     if (v === 1) map.set(k, 100);
